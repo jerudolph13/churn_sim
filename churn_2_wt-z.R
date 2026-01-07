7b7c48ae4a632858519a1cd1efbe43a4dd0a717f
Jacqueline Rudolph <53434512+jerudolph13@users.noreply.github.com>
2026-01-07 10:26:28 -0500

    Update churn_2_wt-z.R

--- churn_2_wt-z.R
+++ churn_2_wt-z.R
@@ -20,7 +20,7 @@ for (package in packages){
 
 nsim <- 1000
 
-model <- "dag4.1" # "dag3", "dag3.1", "dag4", "dag4.1"
+model <- "dag4.1" #"dag1", "dag2.1", "dag2.2", "dag2.3", "dag2.4", "dag3", "dag3.1", "dag4", "dag4.1"
 for (outcome in c("transient", "permanent", "repeated")) {
 
 
@@ -200,5 +200,6 @@ summ.res <- all.res %>%
   ungroup()
 
 write_csv(summ.res, paste0("../results/", model, "_", outcome, "_wt-z_summ.csv"))
+
 
 }
