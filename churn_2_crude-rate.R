b0696c9abd59e0aea711aa6f7f0817d5c339f1e1
Jacqueline Rudolph <53434512+jerudolph13@users.noreply.github.com>
2026-01-07 10:25:12 -0500

    Update churn_2_crude-rate.R
    Put all DAGs back in the loop

--- churn_2_crude-rate.R
+++ churn_2_crude-rate.R
@@ -6,7 +6,8 @@ for (package in packages){
 
 nsim <- 1000
 
-for (model in c("dag4.1")) { #"dag2.2", "dag2.3", "dag2.4"
+for (model in c("dag1", "dag2.1", "dag2.2", "dag2.3", "dag2.4",
+                "dag3", "dag3.1", "dag4", "dag4.1")) { 
 for (outcome in c("transient", "permanent", "repeated")) {
 
 
