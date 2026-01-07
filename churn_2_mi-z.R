f8c5df957b63b59b8d6e3816b1edce25cbe11987
Jacqueline Rudolph <53434512+jerudolph13@users.noreply.github.com>
2026-01-07 10:26:55 -0500

    Update churn_2_mi-z.R

--- churn_2_mi-z.R
+++ churn_2_mi-z.R
@@ -27,7 +27,7 @@ M <- 50       # Number of imputations
 
 set.seed(123)
 
-model <- "dag4.1"
+model <- "dag4.1" #"dag1", "dag2.1", "dag2.2", "dag2.3", "dag2.4", "dag3", "dag3.1", "dag4", "dag4.1"
 
 for (outcome in c("transient", "permanent", "repeated")) { 
   
