7633d051625d830e2a6ae7e18e32855786a591ee
Jacqueline Rudolph <53434512+jerudolph13@users.noreply.github.com>
2026-01-07 10:26:41 -0500

    Update churn_2_wt.R

--- churn_2_wt.R
+++ churn_2_wt.R
@@ -19,7 +19,7 @@ for (package in packages){
 
 nsim <- 1000
 
-model <- "dag4.1"
+model <- "dag4.1" #"dag1", "dag2.1", "dag2.2", "dag2.3", "dag2.4", "dag3", "dag3.1", "dag4", "dag4.1"
 for (outcome in c("transient", "permanent", "repeated")) {
 
 
